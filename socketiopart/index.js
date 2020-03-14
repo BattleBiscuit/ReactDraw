b4e5320be4c417146f81105c7c0512bd6bb668c4
b.seidlel@oth-aw.de <b.seidel@oth-aw.de>
2020-03-14 01:18:40 +0100

    beginn Backend

---
+++ socketiopart/index.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const http = require("http");
+const socketIo = require("socket.io");
+
+//Port from environment variable or default - 4001
+const port = process.env.PORT || 4001;
+
+//Setting up express and adding socketIo middleware
+const app = express();
+const server = http.createServer(app);
+const io = socketIo(server);
+
+//Setting up a socket with the namespace "connection" for new sockets
+io.on("connection", socket => {
+    console.log('user connected');
+
+    socket.on('update image', img => {
+      io.broadcast.emit(image);
+    })
+
+    //A special namespace "disconnect" for when a client disconnects
+    socket.on("disconnect", () => console.log("Client disconnected"));
+});
+
+server.listen(port, () => console.log(`Listening on port ${port}`));
